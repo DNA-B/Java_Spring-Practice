f806af1360be86cfeba210d00f79abc3cf163f2a
DNA-B <sphz3813@gmail.com>
2024-03-26 02:37:00 +0900

    Chapter 11

--- practice-frontend/todo-app/src/component/counter/ResetButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function ResetButton({ resetMethod }) {
-  return (
-    <div className="ResetButton">
-      <button className="resetButton" onClick={() => resetMethod(0)}>
-        RESET
-      </button>
-    </div>
-  );
-}
--- practice-frontend/todo-app/src/component/todo/ErrorComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function ErrorComponent() {
-  return (
-    <div className="ErrorComponent">
-      <h2>We are working really hard!</h2>
-      <div>Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.</div>
-    </div>
-  );
-}
